12086c08e0e0acd0d3e19b818852a14ba6b11dc3
MOACIR <mlameu.1976@gmail.com>
2026-04-16 01:16:45 -0300

    fix: ensure models load order with base module

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from app.models import *
+from app.models import base
 from sqlalchemy.orm import configure_mappers
 
 configure_mappers()
---
+++ app/models/base.py
@@ -0,0 +1,4 @@
+from app.models.match import Match
+from app.models.league import League
+from app.models.opportunity import Opportunity
+from app.models.pre_match_features import PreMatchFeatures
