2fc870869e9bdeee85811a119a9d3027fb29759f
MOACIR <mlameu.1976@gmail.com>
2026-04-16 00:06:33 -0300

    fix: ensure all models are loaded for SQLAlchemy relationships

--- app/main.py
+++ app/main.py
@@ -2,7 +2,8 @@ from fastapi import FastAPI
 
 from app.database import Base, engine
 
-Base.metadata.drop_all(bind=engine)
+from app.models import *
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
---
+++ app/models/__init__.py
@@ -0,0 +1,4 @@
+from .match import Match
+from .league import League
+from .opportunity import Opportunity
+from .pre_match_features import PreMatchFeatures
