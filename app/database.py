fe6deff0a6010011c5d151f8c4d1950c2a0b23f8
MOACIR <mlameu.1976@gmail.com>
2026-04-25 21:11:55 -0300

    fix: usar DATABASE_URL do render

--- app/database.py
+++ app/database.py
@@ -1,15 +1,13 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-
-DATABASE_URL = "postgresql://postgres:452576@localhost:5432/futanalysis"
-
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
     echo=False
 )
-
 
 SessionLocal = sessionmaker(
     autocommit=False,
@@ -17,14 +15,10 @@ SessionLocal = sessionmaker(
     bind=engine
 )
 
-
 Base = declarative_base()
 
-
 def get_db():
-
     db = SessionLocal()
-
     try:
         yield db
     finally:
